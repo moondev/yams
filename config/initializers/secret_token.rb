0ebbe7b9c9fb30ea540b7a352fc18a7d814465c0
Chad Moon <chad@skookum.com>
2012-11-22 12:20:17 -0500

    app scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yams::Application.config.secret_token = '98ba9b515387fc4f79faca6b4c4bd00e0b3ae3935c7cb577fc2fb2cd8a1c1a86218301ed4dc7f6d57711cdfeb7e8df973d0757f5fd685757fc4bed0ddbdd17f7'
